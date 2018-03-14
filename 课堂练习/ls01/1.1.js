226201642d54194768fc210f298878ab1b9258eb
pankaiya <3131225624@qq.com>
2018-03-14 23:16:57 +0800

    Signed-off-by: pankaiya <3131225624@qq.com>

---
+++ 课堂练习/ls01/1.1.js
@@ -0,0 +1,18 @@
+
+    //typeof类型检测
+    console.log(typeof 123);//number
+    console.log(typeof true);//boolean
+    console.log(typeof "abc");//string
+    console.log(typeof null);//object
+    console.log(typeof Null);
+    console.log(typeof undefined);//undefined
+    console.log(typeof {name:"Mike",age:20});//object
+
+    console.log(typeof function foo(){});//function
+    //instanceof类型检测
+    var a={name:"Mike",age:20};
+    console.log(a instanceof Object);//true
+
+    
+    
+
