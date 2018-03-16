22a13b16b26e6ea50b0c33429fcc994077e14a12
pankaiya <3131225624@qq.com>
2018-03-16 18:04:17 +0800

    类型检测

--- 课堂练习/ls01/1.1.js
+++ 课堂练习/ls01/1.1.js
@@ -3,15 +3,30 @@
     console.log(typeof 123);//number
     console.log(typeof true);//boolean
     console.log(typeof "abc");//string
-    console.log(typeof null);//object
-    console.log(typeof Null);
-    console.log(typeof undefined);//undefined
     console.log(typeof {name:"Mike",age:20});//object
-
+    console.log(typeof null);//object
+    console.log(typeof Null);//undefined
+    console.log(typeof undefined);//undefined
+    
     console.log(typeof function foo(){});//function
+    console.log(typeof Array);//function
+    console.log(typeof Function);//function
+    console.log(typeof Date);//function
+    console.log(typeof Number);//function
     //instanceof类型检测
     var a={name:"Mike",age:20};
     console.log(a instanceof Object);//true
+
+    var b = [12,34,{},""];
+    console.log(b instanceof Array);//true
+    console.log(b instanceof Object);//true
+
+    var Person = function(){
+        //...
+    };
+    var p1 = new Person();
+    console.log(p1 instanceof Person);//true
+    console.log(p1 instanceof Object);//true
 
     
     
