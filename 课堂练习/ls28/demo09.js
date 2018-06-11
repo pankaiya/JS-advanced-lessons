d2d365492c534f184dceaa5c2044de72ce6688ec
pankaiya <3131225624@qq.com>
2018-06-11 20:52:05 +0800

    解构赋值

--- 课堂练习/ls28/demo09.js
+++ 课堂练习/ls28/demo09.js
@@ -31,10 +31,10 @@ console.log(foo2,fee2);
 
 //不完全解构的情况
 let [x2, y2] = [1, 2, 3];
-console.log(x2, y2);
+console.log(x2, y2);//1 2
 
 let [a2, [b2], d2] = [1, [2, 3], 4];
-console.log(a2, b2, d2);
+console.log(a2, b2, d2);//1 2 4
 
 //如果等号的右边不是数组（或者严格地说，不是可遍历的结构，参见《Iterator》一章），那么将会报错
 // let [foo] = 1;
@@ -59,7 +59,7 @@ function f2() {
     return 2;
 }
 let [x7 = f2()] = [1];
-console.log(x7);
+console.log(x7);//1
 
 //默认值可以引用解构赋值的其他变量，但该变量必须已经声明
 let [m1 = 1, n1 = m1] = []; // m1=1; n1=1
@@ -72,12 +72,12 @@ console.log(m1,n1,m2,n2,m3,n3);
 let a = [];
 let b=[2,3,4];
 [a[0],a[1],a[2]] = b;
-console.log(a == b);//false or true
+console.log(a == b);//false(因为是数组，所以注意判等，比较的是内存空间)
 
 let a = [];
 let b=[2,3,4];
 a = b;
-console.log(a == b);//false or true
+console.log(a == b);// true
 
 //以下内容先只做了解，详细内容参见Set Map和Generator章节
 //对于Set结构，也可以使用数组的解构赋值
--- 课堂练习/ls28/demo11.js
+++ 课堂练习/ls28/demo11.js
@@ -13,7 +13,7 @@ console.log(len); // 5
 
 //解构赋值时，如果等号右边是数值和布尔值，则会先转为对象。
 let {toString: s1} = 123;
-console.log(s1); //
+console.log(s1); //function toString(){[native code]}
 //s1 === Number.prototype.toString // true
 
 let {toString: s2} = true;
